c172a623054c991867885f448b90421acc0cf169
Guilherme <140681981+Placido-dev@users.noreply.github.com>
2025-01-15 21:36:03 -0300

    Update DEF.py

---
+++ DEF.py
@@ -0,0 +1,14 @@
+# def imprimir(a, b, c) <- parametros: 
+#     print(a, b, c) 
+
+
+# imprimir(1, 2, 3) <- argumentos
+# imprimir(4, 5, 6) <- argumentos
+
+def saudacao(nome='Sem Nome'):
+    print(f'Olá, {nome}!')
+
+
+saudacao('Segredo')
+saudacao('Guilherme')
+saudacao()
