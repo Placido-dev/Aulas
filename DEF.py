26780b5dbd7516bcf71f5ba8bab18c682897674e
Guilherme <140681981+Placido-dev@users.noreply.github.com>
2025-01-15 21:55:20 -0300

    Update DEF.py

--- DEF.py
+++ DEF.py
@@ -5,6 +5,16 @@
 # imprimir(1, 2, 3) <- argumentos
 # imprimir(4, 5, 6) <- argumentos
 
+def separacao():
+    print('-' * 35)
+
+def aula(numero_aula=0):
+    print(f'Aula {numero_aula}')
+
+
+aula(1)
+separacao()
+
 def saudacao(nome='Sem Nome'):
     print(f'Olá, {nome}!')
 
@@ -12,3 +22,18 @@ def saudacao(nome='Sem Nome'):
 saudacao('Segredo')
 saudacao('Guilherme')
 saudacao()
+
+separacao()
+aula(2)    
+separacao()    
+
+def soma(x, y):
+    print(x + y)
+    # print(x * y)
+
+soma(1, 2)    
+soma(1, 56)
+
+separacao()
+aula(3)
+separacao()
