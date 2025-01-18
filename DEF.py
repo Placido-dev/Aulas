06247d2691b87346a901dd26051b7ca1f511697a
Guilherme <140681981+Placido-dev@users.noreply.github.com>
2025-01-18 18:23:54 -0300

    Update DEF.py

--- DEF.py
+++ DEF.py
@@ -50,3 +50,23 @@ soma2(3, 7, 0)
 separacao()
 aula(4)
 separacao()
+
+
+x = 1
+
+def escopo():
+    global x
+    x = 10
+
+    def outra_funcao():
+        global x
+        x = 11
+        y = 2
+        print(x, y)
+
+    outra_funcao()    
+    print(x)
+
+print(x)
+escopo()
+print(x)    
